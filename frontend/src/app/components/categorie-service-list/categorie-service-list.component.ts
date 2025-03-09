603893dfeaa7f0d144d49e86ce9ac434b3ff5ee2
alex <rtovonavalona@gmail.com>
2025-03-09 22:30:24 +0300

    crud

--- frontend/src/app/components/categorie-service-list/categorie-service-list.component.ts
+++ frontend/src/app/components/categorie-service-list/categorie-service-list.component.ts
@@ -1,13 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { CategorieServiceService } from '../../services/categorie-service.service';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-categorie-service-list',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './categorie-service-list.component.html',
   styleUrl: './categorie-service-list.component.css'
 })
 export class CategorieServiceListComponent implements OnInit {
+
+  newCategorie = { description: '' };
 
   categories: any[] = [];
   constructor(private categorieService: CategorieServiceService) { }
@@ -18,6 +23,17 @@ export class CategorieServiceListComponent implements OnInit {
     this.categorieService.getCategories().subscribe(data => this.categories =
       data);
   }
+
+  addCategorie(): void {
+    if (this.newCategorie.description) {
+      this.categorieService.addCategorie(this.newCategorie).subscribe(() => {
+        this.loadCategories(); // Recharge la liste après ajout
+        this.newCategorie = { description: '' }; // Réinitialise le formulaire
+      });
+    }
+  }
+
+
   // deleteArticle(id: string): void {
   //   this.articleService.deleteArticle(id).subscribe(() =>
   //     this.loadArticles());
