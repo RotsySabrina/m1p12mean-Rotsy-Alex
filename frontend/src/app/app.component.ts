9ebcf37cea97bb1e4bad262bf7aa9b64ca9a396a
alex <rtovonavalona@gmail.com>
2025-03-10 22:35:45 +0300

    template

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';  // Chemin à vérifier
+import { FooterComponent } from './components/footer/footer.component';  // Chemin à vérifier
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],  // Ajout des composants
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'frontend';
