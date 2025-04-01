f841a908318d8798e0783d29c2216634b279fc84
alex <rtovonavalona@gmail.com>
2025-04-01 14:55:47 +0300

    auth

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://api.monsite.com' // URL pour la production
-   };
+    apiUrl: 'https://m1p12mean-rotsy-alex.onrender.com' // URL pour la production
+};
